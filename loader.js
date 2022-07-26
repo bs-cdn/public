776e3d42090c5e972d364926547ab2e8d7900a68
ektucdn <106139174+ektucdn@users.noreply.github.com>
2022-07-26 18:44:46 +0530

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,4 +1,4 @@
-document.querySelector(".total_hits").innerHTML = 14022309+82038+3522;
-document.querySelector(".unique_hits").innerHTML = 4189972+8367+304;
-document.querySelector(".today_hits").innerHTML = 3522;
+document.querySelector(".total_hits").innerHTML = 14022309+82038+22159;
+document.querySelector(".unique_hits").innerHTML = 4189972+8367+2353;
+document.querySelector(".today_hits").innerHTML = 22159;
 
