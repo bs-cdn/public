bab08b897721e15e2a5ca1973d14d7e712a07550
ektucdn <106139174+ektucdn@users.noreply.github.com>
2022-12-02 10:45:23 +0530

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
-document.querySelector(".total_hits").innerHTML = 18977502+23244+35235+36220+17261+32776+17472+22832+17449+29221+32019+30732;
-document.querySelector(".unique_hits").innerHTML = 4692700+2769+3860+3642+1608+3366+1573+2191+1563+2975+3149+3153;
-document.querySelector(".today_hits").innerHTML = 30732;
+document.querySelector(".total_hits").innerHTML = 18977502+23244+35235+36220+17261+32776+17472+22832+17449+29221+32019+30732+30486;
+document.querySelector(".unique_hits").innerHTML = 4692700+2769+3860+3642+1608+3366+1573+2191+1563+2975+3149+3153+3136;
+document.querySelector(".today_hits").innerHTML = 30486;
    
  
  
