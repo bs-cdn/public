5d8d2a8179bbb9c484fb3a4e669efacb1314d118
ektucdn <106139174+ektucdn@users.noreply.github.com>
2023-01-25 11:23:01 +0530

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
- document.querySelector(".total_hits").innerHTML = 20631399+21981;
-document.querySelector(".unique_hits").innerHTML = 4863469+2417;
-document.querySelector(".today_hits").innerHTML = 21981;
+ document.querySelector(".total_hits").innerHTML = 20631399+21981+22700;
+document.querySelector(".unique_hits").innerHTML = 4863469+2417+2351;
+document.querySelector(".today_hits").innerHTML = 22700;
     
    
   
