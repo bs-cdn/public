20498c36d751d6819f585c276a8b2b4f72c29916
ektucdn <106139174+ektucdn@users.noreply.github.com>
2023-01-30 10:36:07 +0530

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,7 +1,3 @@
- document.querySelector(".total_hits").innerHTML = 20631399+21981+22700;
-document.querySelector(".unique_hits").innerHTML = 4863469+2417+2351;
-document.querySelector(".today_hits").innerHTML = 22700;
-    
-   
-  
- 
+document.querySelector(".total_hits").innerHTML = 20631399+21981+22700+17341+9658+17427+14645+11196;
+document.querySelector(".unique_hits").innerHTML = 4863469+2417+2351+1842+946+1809+1403+1054;
+document.querySelector(".today_hits").innerHTML = 11196;
